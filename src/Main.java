e81c6ee6ea99935d94ea9e643b2bf5b25209c415
DemitriSimoes <demitrisch@gmail.com>
2025-03-13 23:26:41 -0300

    Class construction

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,16 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello world!");
+        class Caneta{
+            String cor;
+            Boolean tampa;
+            void tampar() {
+                if (tampa = true) {
+                    System.out.println("Caneta tampada");
+                } else {
+                    System.out.println("Caneta destampada");
+                }
+            }
+        }
     }
 }
